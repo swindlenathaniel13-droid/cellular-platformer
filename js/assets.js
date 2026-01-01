fb668c4d7b5b7320a5d001cabca954a8dba1bd0c
swindlenathaniel13-droid <swindlenathaniel13@gmail.com>
2026-01-01 14:25:07 -0800

    Update assets.js

--- js/assets.js
+++ js/assets.js
@@ -1,8 +1,4 @@
-// js/assets.js
-const CACHE_BUST = "v2026-01-01a";
-const ASSET_BASE = "./assets/";
-
-export const FILES = {
+const FILES = {
   bg: "Background_Pic.png",
   platform: "Platform.png",
   door: "Exit_Door.png",
@@ -13,31 +9,36 @@ export const FILES = {
   enemy1: "Enemy1.png",
   enemy2: "Enemy2.png",
 
-  dash: "Powerup_Dash.png",
-  speed: "Powerup_Speedboost.png",
-  phone: "powerup_homephone.png",
-
   nate: "Nate.png",
   kevin: "Kevin.png",
   scott: "Scott.png",
   gilly: "Gilly.png",
-  edgar: "Edgar.png"
+  edgar: "Edgar.png",
 };
 
-function safeUrl(file){
-  return `${ASSET_BASE}${file}?${CACHE_BUST}`;
+function assetBase() {
+  // robust on GitHub Pages subpaths
+  return new URL("../assets/", import.meta.url).toString();
 }
 
-function loadImageWithTimeout(file, timeoutMs = 6000){
-  const url = safeUrl(file);
+export function getFiles() {
+  return { ...FILES };
+}
+
+export function assetUrl(file) {
+  return assetBase() + file;
+}
+
+function loadImage(file, timeoutMs = 6000) {
+  const url = assetUrl(file);
 
   return new Promise((resolve) => {
     const img = new Image();
-    let finished = false;
+    let done = false;
 
     const finish = (ok, reason) => {
-      if (finished) return;
-      finished = true;
+      if (done) return;
+      done = true;
       resolve({ ok, img: ok ? img : null, file, url, reason });
     };
 
@@ -45,31 +46,29 @@ function loadImageWithTimeout(file, timeoutMs = 6000){
 
     img.onload = () => { clearTimeout(t); finish(true, "ok"); };
     img.onerror = () => { clearTimeout(t); finish(false, "error"); };
-
     img.src = url;
   });
 }
 
-export async function loadAssets(onProgress){
-  const keys = Object.keys(FILES);
-  const total = keys.length;
+export async function loadAssets(onProgress) {
+  const entries = Object.entries(FILES);
+  const total = entries.length;
 
   const assets = {};
   const missing = [];
-  let done = 0;
+  let loaded = 0;
 
-  for (const k of keys){
-    const file = FILES[k];
-    onProgress?.({ done, total, file });
+  for (const [key, file] of entries) {
+    onProgress?.({ loaded, total, file });
 
-    const res = await loadImageWithTimeout(file, 6000);
+    const res = await loadImage(file);
+    loaded++;
 
-    done++;
-    onProgress?.({ done, total, file });
+    onProgress?.({ loaded, total, file });
 
-    if (res.ok) assets[k] = res.img;
+    if (res.ok) assets[key] = res.img;
     else missing.push(`${file} (${res.reason})`);
   }
 
-  return { assets, missing, total };
+  return { assets, missing };
 }
