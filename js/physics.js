1960c90c74a96cad16b841859015964979bf3ff3
swindlenathaniel13-droid <swindlenathaniel13@gmail.com>
2026-01-01 17:03:51 -0800

    Update physics.js

--- js/physics.js
+++ js/physics.js
@@ -1,37 +1,30 @@
-import { rectsOverlap } from "./utils.js";
-import { CONFIG } from "./config.js";
+// js/physics.js
+import { aabb } from "./utils.js";
 
-export function moveAndCollide(body, solids, dt) {
-  const steps = Math.min(CONFIG.dev.maxSubSteps, Math.max(1, Math.ceil(dt / CONFIG.dev.fixedDt)));
-  const stepDt = dt / steps;
-
+export function resolvePlatforms(body, platforms){
   body.onGround = false;
-  body.hitCeil = false;
 
-  for (let i = 0; i < steps; i++) {
-    // X axis
-    body.x += body.vx * stepDt;
-    for (const s of solids) {
-      if (!rectsOverlap(body, s)) continue;
-      if (body.vx > 0) body.x = s.x - body.w;
-      else if (body.vx < 0) body.x = s.x + s.w;
-      body.vx = 0;
-    }
+  // Horizontal
+  body.x += body.vx * body.dt;
+  for (const p of platforms){
+    if (!aabb(body, p)) continue;
+    if (body.vx > 0) body.x = p.x - body.w;
+    else if (body.vx < 0) body.x = p.x + p.w;
+    body.vx = 0;
+  }
 
-    // Y axis
-    body.y += body.vy * stepDt;
-    for (const s of solids) {
-      if (!rectsOverlap(body, s)) continue;
+  // Vertical
+  body.y += body.vy * body.dt;
+  for (const p of platforms){
+    if (!aabb(body, p)) continue;
 
-      if (body.vy > 0) {
-        body.y = s.y - body.h;
-        body.vy = 0;
-        body.onGround = true;
-      } else if (body.vy < 0) {
-        body.y = s.y + s.h;
-        body.vy = 0;
-        body.hitCeil = true;
-      }
+    if (body.vy > 0){
+      body.y = p.y - body.h;
+      body.vy = 0;
+      body.onGround = true;
+    } else if (body.vy < 0){
+      body.y = p.y + p.h;
+      body.vy = 0;
     }
   }
 }
